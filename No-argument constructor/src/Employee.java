32602fe6eac195abf93a3d03ce50decf8ca438e1
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-04-14 19:19:11 +0530

    first commit

---
+++ No-argument constructor/src/Employee.java
@@ -0,0 +1,21 @@
+
+public class Employee {
+
+	public static void main(String[] args) {
+		int id;
+		int name;
+		int salary;
+	
+			System.out.println("user defined no-argument" + "constructor executed");
+	}
+				
+	private String salary;
+	void display()
+				{
+		String id=null;
+		String name=null;
+		System.out.println(id + "" + name + "" + salary);
+		
+	}
+
+}
