14497dff1f2cb32d6485fa3f5f2011ab28b06d06
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-04-14 23:33:01 +0530

    first commit

---
+++ Parameterized constructor/src/Employee.java
@@ -0,0 +1,40 @@
+
+public class Employee {
+	
+
+
+	public static void Employee(String string) 
+	{
+		
+        int id1=0;
+		int name1=0;
+		int salary1=0;
+		Employee ("id1, String name1, float salary1");  
+		
+			System.out.println("user defined no-argument" + "constructor executed");
+			display employee = new display();
+	        System.out.println(id1 + "" + name1 + "" + salary1);
+			  
+
+	}
+}
+		
+	
+			
+			
+
+
+	
+
+
+
+  
+
+
+
+
+		 
+	
+
+	
+	
