dff0688ae8fa4526036a4bdeee98125023257afe
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-04-20 18:33:16 +0530

    first commit

---
+++ test2/src/access_modifiers_demo/test2.java
@@ -0,0 +1,14 @@
+package access_modifiers_demo;
+public class test2 {
+	private char[] i;
+	       public static void main(String[] args) {
+    	   test2 t2=new test2();
+    	   t2.show();
+                 System.out.println(t2.i);
+		}
+             private void show() {
+		
+		
+	}
+
+}
