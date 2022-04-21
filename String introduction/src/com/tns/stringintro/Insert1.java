f405f7dcd0e89fa39c41c6c731ececa20e68e5f9
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-04-21 11:20:56 +0530

    first commit

---
+++ String introduction/src/com/tns/stringintro/Insert1.java
@@ -0,0 +1,17 @@
+package com.tns.stringintro;
+
+public class Insert1 {
+
+	public static void main(String[] args) {
+		StringBuffer s = new StringBuffer();
+		StringBuffer s1 = new StringBuffer("Hello JAVA");
+		StringBuffer s2 = new StringBuffer("Hello");
+		StringBuffer s3 = new StringBuffer("Hello");
+		
+		
+		System.out.println(s1.reverse());
+		System.out.println(s.capacity());
+		System.out.println(s1.capacity());
+		
+	}
+}
