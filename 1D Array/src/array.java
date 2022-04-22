c2e8bf17c710178db192d05d69565549a610f430
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-04-22 11:09:00 +0530

    first commit

---
+++ 1D Array/src/array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class array 
+{
+
+	public static void main(String[] args) 
+	{
+		int[] arr = {12,4,5,2,5};
+		System.out.println(arr);
+		for(int i = 0; i < arr.length; i++) 
+		{
+			System.out.println(arr[i]+" ");
+		}
+		
+
+	}
+
+}
