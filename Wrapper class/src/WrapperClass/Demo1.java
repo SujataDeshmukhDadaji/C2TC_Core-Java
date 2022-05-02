78b0fef715e5e18e08c604a78ec6308a92c843fa
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-05-02 11:41:33 +0530

    first commit

---
+++ Wrapper class/src/WrapperClass/Demo1.java
@@ -0,0 +1,38 @@
+package WrapperClass;
+
+public class Demo1 {
+
+	public static void main(String[] args) 
+	{
+		byte b=10;
+		short s=20;
+		int i=30;
+		long l=40;
+		float f=50.0F;
+		double d=60.0D;
+		char c='a';
+		boolean b2=true;
+		
+		System.out.println("...Printing object values...");
+		int byteobj = 10;
+		System.out.println("byte object: "+byteobj);
+		int shortobj = 20;
+		System.out.println("short object: "+shortobj);
+		int intobj = 30;
+		System.out.println("integer object: "+intobj);
+		int longobj = 40;
+		System.out.println("long object: "+longobj);
+		float floatobj = 50.0F;
+		System.out.println("float object: "+floatobj);
+		double doubleobj = 60.0D;
+		System.out.println("double object: "+doubleobj);
+		char charobj = 'a';
+		System.out.println("character object: "+charobj);
+		boolean booleanobj = true;
+		System.out.println("boolean object: "+booleanobj);
+		
+		
+
+	}
+
+}
