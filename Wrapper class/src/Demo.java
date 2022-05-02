6871c65dbea4aa8f45ea70b1caa930817e0777b2
Vishal Deshmukh <Vishal Deshmukh@DESKTOP-0G44UBS>
2022-05-02 11:03:12 +0530

    first commit

---
+++ Wrapper class/src/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		int a=20;
+		Integer i=Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+" "+i+" "+j);
+		
+
+	}
+
+}
